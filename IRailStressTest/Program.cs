cfcdfc655170c1301bbbfc1b5563e41b58820387
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-14 12:55:35 +0100

    HttpS

--- IRailStressTest/Program.cs
+++ IRailStressTest/Program.cs
@@ -108,7 +108,7 @@ namespace IRailStressTest
 
             depTime.AddMinutes(timeDrift);
 
-            return $"http://api.irail.be/connections?to={arr}&" +
+            return $"https://api.irail.be/connections?to={arr}&" +
                    $"from={dep}&" +
                    $"date={depTime:ddMMyy}&time={depTime:HHmm}&" +
                    $"timeSel=depart";
